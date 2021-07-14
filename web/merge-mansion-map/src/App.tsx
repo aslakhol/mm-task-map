11276a0ce056756a55b714743f72e4e3901dfc8d
Aslak Hollund <aslakhol@gmail.com>
2021-07-14 20:54:50 +0200

    second style canvas

--- web/merge-mansion-map/src/App.tsx
+++ web/merge-mansion-map/src/App.tsx
@@ -3,17 +3,25 @@ import "./App.css";
 import Canvas from "./Canvas";
 
 const App = () => {
-  const draw = (ctx: any) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.beginPath();
-    ctx.arc(50, 100, 20 * Math.sin(100 * 0.05) ** 2, 0, 2 * Math.PI);
-    ctx.fill();
+  const draw = (context: CanvasRenderingContext2D) => {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    context.fillStyle = "#000000";
+    context.beginPath();
+    context.arc(50, 100, 20 * Math.sin(100 * 0.05) ** 2, 0, 2 * Math.PI);
+    context.fill();
+  };
+
+  const draw2 = (context: CanvasRenderingContext2D) => {
+    context.fillStyle = "rgb(200, 0, 0)";
+    context.fillRect(10, 10, 50, 50);
+
+    context.fillStyle = "rgba(0, 0, 200, 0.5)";
+    context.fillRect(30, 30, 50, 50);
   };
 
   return (
     <div className="App">
-      <Canvas draw={draw} />
+      <Canvas draw={draw} height={100} width={100} />
     </div>
   );
 };
--- web/merge-mansion-map/src/Canvas.tsx
+++ web/merge-mansion-map/src/Canvas.tsx
@@ -1,11 +1,24 @@
-import React from "react";
-import useCanvas from "./useCanvas";
+import React, { useRef, useEffect } from "react";
 
-const Canvas = (props: any) => {
-  const { draw, ...rest } = props;
-  const canvasRef = useCanvas(draw);
+const Canvas = (props: {
+  draw: (context: CanvasRenderingContext2D) => void;
+  height: number;
+  width: number;
+}) => {
+  const { draw, height, width } = props;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  return <canvas ref={canvasRef} {...rest} />;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d") ?? null;
+
+    if (canvas === null || context === null) {
+      throw new Error("couldn't find canvas");
+    }
+    draw(context);
+  });
+
+  return <canvas ref={canvasRef} height={height} width={width} />;
 };
 
 export default Canvas;
