79d14b4760c145d478f59969d102967544f1e08f
Aslak Hollund <aslakhol@gmail.com>
2021-11-29 13:59:34 +0100

    add name to areas in filter

--- web/src/Filter/Filter.tsx
+++ web/src/Filter/Filter.tsx
@@ -17,22 +17,22 @@ const Filter = (props: Props) => {
       className="filter"
     >
       <option value="all">All</option>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-      <option value="13">13</option>
-      <option value="14">14</option>
-      <option value="15">15</option>
-      <option value="16">16</option>
+      <option value="1">1 The Grand Drive</option>
+      <option value="2">2 Mansion Stairs</option>
+      <option value="3">3 Tranquillity Terrace</option>
+      <option value="4">4 Frog Pond Falls</option>
+      <option value="5">5 Garage</option>
+      <option value="6">6 Beach House</option>
+      <option value="7">7 Side Entrance</option>
+      <option value="8">8 Harbour</option>
+      <option value="9">9 Flower Garden</option>
+      <option value="10">10 Stone Garden</option>
+      <option value="11">11 Pool Area</option>
+      <option value="12">12 The Grand Drive 2</option>
+      <option value="13">13 Fortress</option>
+      <option value="14">14 Plaza</option>
+      <option value="15">15 Pier</option>
+      <option value="16">16 The Old Well</option>
     </select>
   );
 };
